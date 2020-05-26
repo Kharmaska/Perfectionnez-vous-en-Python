00a2a72c76bab621f7c1d86942e0814af0ead903
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-05-26 18:09:45 +0200

    set of MPs part done

--- parite.py
+++ parite.py
@@ -2,39 +2,68 @@
 # coding: utf-8
 import argparse
 import logging as lg
-
+import os
+import pandas as pd
 import analysis.csv as c_an
 import analysis.xml as x_an
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
-        information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""Kind of file to analyse. Is it a CSV or an XML?""")
-    parser.add_argument("-v", "--verbose", action='store_true', help="""Make the application talk!""")
-    return parser.parse_args()
+# def parse_arguments():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
+#         information about the members of parliament""")
+#     parser.add_argument("-e", "--extension", help="""Kind of file to analyse. Is it a CSV or an XML?""")
+#     parser.add_argument("-v", "--verbose", action='store_true', help="""Make the application talk!""")
+#     return parser.parse_args()
+  
 
-def main():
-    args = parse_arguments()
-    if args.verbose:
-        lg.basicConfig(level=lg.DEBUG)
+class SetOfParliamentMembers:
+    def __init__(self,name):
+      self.name = name
+    
+    def data_from_csv(self, csv_file):
+      self.dataframe = pd.read_csv("data/current_mps.csv", sep=";")
+      
+    def data_from_dataframe(self, dataframe):
+      self.dataframe = dataframe
+      
+    def display_chart(self):
+      pass
+    
+    def split_by_political_party(self):
+      result = {}
+      data = self.dataframe
+      
+      all_parties = data["parti_ratt_financier"].dropna().unique()
+      
+      for party in all_parties:
+        data_subset = data[data.parti_ratt_financier == party]
+        subset = SetOfParliamentMembers('MPs from party "{}"'.format(party))
+        subset.data_from_dataframe(data_subset)
+        result[party] = subset
 
-    try:
-        datafile = args.datafile
-        if datafile == None:
-            raise Warning('You must indicate a datafile!')
-        else:
-            try:
-                if args.extension == 'xml':
-                    x_an.launch_analysis(datafile)
-                elif args.extension == 'csv':
-                    c_an.launch_analysis(datafile)
-            except FileNotFoundError as e:
-                print("Ow :( The file was not found. Here is the original message of the exception :", e)
-            finally:
-                lg.info('#################### Analysis is over ######################')
-    except Warning as e:
-        lg.warning(e)
+      return result
 
+def main():    
+    # # args = parse_arguments()
+    # if args.verbose:
+    #     lg.basicConfig(level=lg.DEBUG)
+
+    # try:
+    #     datafile = args.datafile
+    #     if datafile == None:
+    #         raise Warning('You must indicate a datafile!')
+    #     else:
+    #         try:
+    #             if args.extension == 'xml':
+    #                 x_an.launch_analysis(datafile)
+    #             elif args.extension == 'csv':
+    #                 c_an.launch_analysis(datafile)
+    #         except FileNotFoundError as e:
+    #             print("Ow :( The file was not found. Here is the original message of the exception :", e)
+    #         finally:
+    #             lg.info('#################### Analysis is over ######################')
+    # except Warning as e:
+    #     lg.warning(e)
+  print(SetOfParliamentMembers(LR))
 if __name__ == "__main__":
     main()
