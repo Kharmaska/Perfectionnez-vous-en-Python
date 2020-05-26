ee6bb21c72126a68eb954808d81cd4c1d1bfe91b
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-05-26 10:53:34 +0200

    created arg parser for xml & CSV

--- parite.py
+++ parite.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python3
 # coding: utf-8
+import argparse
 
-def main()
-  pass
+import analysis.csv as c_an
+import analysis.xml as x_an
 
-if __name__ = "__main__"
-main()
+def parse_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-e","--extension", help="""Type of file to analyse. Is it a CSV of an XML?""")
+  return parser.parse_args()
+
+def main():
+  args = parse_arguments()
+  if args.extension == 'csv':
+    c_an.launch_analysis('current_mps.csv')
+  elif args.extension == 'xml':
+    x_an.launch_analysis('CRSANR5L15S2017E1N001.xml')
+
+if __name__ == "__main__":
+  main()    
